9fadb6d313d85550fa13e54deab9fa2750d93051
Rafal Wrobel <wrobelrafal1992@gmail.com>
2018-05-05 13:59:52 +0200

    Add routes

--- country-app/src/index.js
+++ country-app/src/index.js
@@ -1,5 +1,23 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store from './store';
+import Navigation from './presentational/navigation.component';
+
+render(
+    <Provider store={store}>
+        <Router history={hashHistory}>
+            <Route path='/' component={Navigation}>
+
+            </Route>
+        </Router>
+    </Provider>,
+    document.getElementById('root')
+);
+
+/*import React from 'react';
+import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import DevTools from './DevTools';
@@ -14,4 +32,4 @@ render(
     </Provider>,
     document.getElementById('root')
 );
-store.dispatch(getCountries());
+store.dispatch(getCountries());*/
---
+++ country-app/src/routes.js
@@ -0,0 +1,12 @@
+
+import React from 'react';
+import { Route } from 'react-router';
+import Navigation from './presentational/navigation.component';
+
+export default (
+//	<Router history={hashHistory}>
+		<Route path='/' component={Navigation}>
+
+		</Route>
+//	</Router>
+);
